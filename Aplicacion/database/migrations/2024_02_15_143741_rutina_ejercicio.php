815fd6ab4f117b50bdc20e7f9d3053267fd08bd5
Rsandez <raulsandezmartinez@gmail.com>
2024-02-15 16:10:39 +0100

    migraciones

---
+++ Aplicacion/database/migrations/2024_02_15_143741_rutina_ejercicio.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rutina_ejercicio', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('rutina_id');
+            $table->foreignId('ejercicio_id');
+            $table->integer('numero_series');
+            $table->integer('numero_repeticiones');
+            $table->integer('rir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
